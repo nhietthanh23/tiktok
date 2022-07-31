2b7157298c0db80dc7b0d71404e2094929c52f7a
NhietThanh <thanhtrieu@chonhagiau.com>
2022-07-31 22:48:38 +0700

    Chia bố cục layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import HomePage from './pages/Home';
-import NewsPage from './pages/News';
-import ContactPage from './pages/Contact';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routers';
 function App() {
     return (
-        <div className="App">
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/news">News</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/news" element={<NewsPage />} />
-                <Route path="/contact" element={<ContactPage />} />
-            </Routes>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={<Page />} />;
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
