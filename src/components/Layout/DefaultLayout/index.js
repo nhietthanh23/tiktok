dbb09d72d70e3464bc1cba087245cba733d552df
NhietThanh <thanhtrieu@chonhagiau.com>
2022-08-02 22:30:35 +0700

    Xây dựng layout

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,20 @@
+import className from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
+import Header from '~/components/Layout/components/Header';
+import Sidebar from './Sidebar';
+
+const cx = className.bind(styles);
+
+function DefaultLayout({ children }) {
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
