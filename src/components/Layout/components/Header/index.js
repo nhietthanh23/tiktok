cebc231e20d4ea312d584c443543a62c84b58407
NhietThanh <thanhtrieu@chonhagiau.com>
2022-08-02 23:24:48 +0700

    Xây dựng header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,27 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCricleXmark } from '@fortawesome/free-solid-svg-icons';
 
+import styles from './Header.module.scss';
+import images from '~/assets/images';
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="search acounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCricleXmark} />
+                    </button>
+                    <button className={cx('search-btn')}></button>
+                </div>
+                <div>hahahah</div>
+            </div>
         </header>
     );
 }
